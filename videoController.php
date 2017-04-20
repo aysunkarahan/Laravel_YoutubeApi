92861812241d3aa36aebef947503af3456559327
aysunkarahan <aysunnkarahan@gmail.com>
2017-04-20 17:29:31 +0300

    Add files via upload

---
+++ videoController.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+class videoController extends Controller {
+	
+
+	public function Liste($pageToken){
+		$url = "https://www.googleapis.com/youtube/v3/search?pageToken=".$pageToken."&key=AIzaSyA3cCIlR0tUnG2FgEHNnN5H6LZNE2fnLpg&channelId=UCSRHQPUYfDLagYzjDuD6xDA&part=snippet,id&order=date&maxResults=10";
+		$data = array();
+		$jurl = file_get_contents($yrl,0,null,null);
+		$json_result = json_decode($y);
+		$data= $json_result->items;
+		$next = $json_result->nextPageToken;
+
+		if(isset($json_result->prevPageToken))
+		{
+			$prev = $json_result->prevPageToken;
+			return view('youtube',array('data'=>$data, 'next'=>$next, 'prev'=>$prev));
+		}
+		return view('youtube',array('data'=>$data, 'next'=>$next));
+		
+		}
+
+	
+
+}
