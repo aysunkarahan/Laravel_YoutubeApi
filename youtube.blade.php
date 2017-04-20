a5e5246a0dfc73a93f7edcaf52ff78b13198c4ec
aysunkarahan <aysunnkarahan@gmail.com>
2017-04-20 17:44:37 +0300

    Update youtube.blade.php

--- youtube.blade.php
+++ youtube.blade.php
@@ -1,30 +1,28 @@
 @extends('app')
 @section('content')
-    
-            <div class="">
+
+          <div class="">
                 @if(isset($data))
                     <div class=""><h1 class="">Youtube sitesine yüklenen videolar</h1></div>
                     @foreach($data as $data_list)
-                        <div class="">
-                            
-                            <div class="" style="background: url({{$lid->snippet->thumbnails->high->url}}) no-repeat center center/100%;">
-                            </div>
+                    <div class="">                            
+                       <div class="" style="background: url({{$lid->snippet->thumbnails->high->url}}) no-repeat center center/100%;"></div>
                             <div class="">
-                                <a href= "{{url('/video/')}}{{'/in/'}}{{$data_list->id->videoId}}"> <h3 class=""><span>
-						            {{$data_list->snippet->title}}<br><span class="widow-killer"></span>
-							</span>
-                                    </h3></a>
-                                <h5 class=class="element-masked-y">
-
-                                    <span>Yuklenme tarihi : {{explode ("T",$data_list->snippet->publishedAt)[0]}}</span></h5>
-
+                                <a href= "{{url('/video/')}}{{'/in/'}}{{$data_list->id->videoId}}"> 
+				<h3 class="">
+				<span>{{$data_list->snippet->title}}<br></span>
+                                    </h3>
+				 </a>
+                                <h5>
+                                    <span>Yuklenme tarihi : {{explode ("T",$data_list->snippet->publishedAt)[0]}}</span>
+				 </h5>
                             </div>
-                        </div>
-                        <br>
+                      </div>
+                      <br>
                      @endforeach
-
-            @endif
-            <div class="">
+                  @endif
+	 </div>
+             <div class="">
                 @if(isset($prev))
                     <a class="" href="{{url("/video/youtube/".$prev)}}">EN YENİ VİDEOLAR</a>
                 @endif
@@ -32,6 +30,5 @@
              <div class="">
              <a class="" href="{{url("/video/youtube/".$next)}}">DAHA ESKİ VİDEOLAR</a>
             </div>
-
-
+	    
 @endsection
